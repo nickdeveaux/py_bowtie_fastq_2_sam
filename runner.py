9e7c8613022bcf8811ec55ba7c5f7580a0ae80a8
Nick De Veaux <nrdeveaux@gmail.com>
2017-07-06 16:17:03 -0400

    runner for July 6th

---
+++ runner.py
@@ -0,0 +1,3 @@
+from py_bowtie_fastq_2_sam import py_bowtie_fastq_2_sam
+
+py_bowtie_fastq_2_sam(folder='/mnt/ceph/users/ndeveaux/th17-jj/th17-jj/cut_n_run/Jason_Data_07_06_2017', data_species='mm10')
